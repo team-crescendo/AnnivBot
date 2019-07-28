a9e613507a8179a389d62937048f7d26f9c03fc0
EunhakLee <admin@return0927.xyz>
2019-07-28 21:43:39 +0900

    Updated client to accept configurations from setting file

--- bot.py
+++ bot.py
@@ -1,16 +1,38 @@
 from discord.ext import commands
+from discord.ext.commands import ExtensionError
+from configparser import ConfigParser
+import logging
+import traceback
+
+
+# Load configuration
+conf = ConfigParser()
+conf.read("settings.ini", encoding="UTF-8")
+Bot = conf["Bot"]
+
+# Load logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(Bot["Name"])
 
 # Define bot
-client = commands.Bot(command_prefix="크센아 ")
-client.name = "AnnivBot"
+client = commands.Bot(command_prefix=Bot["Prefix"])
+client.name = Bot["Name"]
 
 # Register cog extensions
 cogs = [
     "cogs.logger"
     ]
 
+logger.info("Loading {} extensions".format(len(cogs)))
 for cog in cogs:
-    client.load_extension(cog)
+    try:
+        client.load_extension(cog)
+
+    except ExtensionError as ex:
+        logger.warning("Error {} was occured when loading cog extension '{}'\n{}".format(
+            ex.__class__.__name__, cog, traceback.format_exc()
+            ))
+logger.info("Loaded {} extensions".format(len(client.extensions)))
 
 
-
+client.run(Bot["Token"])
